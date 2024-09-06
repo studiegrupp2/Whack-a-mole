16976f6b0b2081037295dc75666251213ade3424
hiersemann <hisseman@gmail.com>
2024-09-06 13:35:25 +0200

    fixat massa grejer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function WhackAMole() {
   return <div>
     
         <Link href="/game"><button className="">START GAME</button></Link>
-        <h1>Hejsan detta är ett test1</h1>
+    <h1>Hejsan detta är ett test1</h1>
+    <h2>mer tester</h2>
     </div>;
 }
