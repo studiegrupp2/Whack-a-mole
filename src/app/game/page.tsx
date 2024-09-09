fc4af047fb9b53090ad6fd10c209be8ee42e3c2d
Ludvig Orest <108837818+LudvigOrest@users.noreply.github.com>
2024-09-09 13:41:50 +0200

    Skapa en "container" som ska innehålla spelplan, timern, knappen och poängräknaren (1) (#5)
    * added container for the game-page and some placeholders while waiting for components to be finished
    
    * lade stylingen i global istället
    
    * changed styling to tailwind

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>pagetest</div>
-  )
-}
+    <div className="min-h-screen min-w-screen flex flex-col items-center">
+      <div className="p-[20px 0px] flex min-w-full justify-around">
+        <div className="game-timer">
+          <div>timer placeholder</div>
+        </div>
+        <div className="game-points">
+          <div>points placeholder</div>
+        </div>
+        <div className="game-start">
+          <div>start button placeholder</div>
+        </div>
+      </div>
+      <div className="game-board">
+        <div>game board placeholder</div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
