4691714abd88d10e9bc46678e24ccdff4edfb894
Ludvig Orest <ludvig.orest@live.se>
2024-09-09 11:47:40 +0200

    added container for the game-page and some placeholders while waiting for components to be finished

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import styles from "./game-page.module.css";
 
 const page = () => {
   return (
-    <div>pagetest</div>
-  )
-}
+    <div className={styles.gameContainer}>
+      <div className={styles.gameTopDiv}>
+        <div className="game-timer">
+          <div>timer placeholder</div>
+        </div>
+        <div className="game-points">
+          <div>points placeholder</div>
+        </div>
+        <div className="game-start">
+          <div>start button placeholder</div>
+        </div>
+      </div>
+      <div className="game-board">
+        <div>game board placeholder</div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
