c3c78ecedd506595318de8c2a52fac8c33df2f07
IsakGunnardo <127835187+IsakGunnardo@users.noreply.github.com>
2024-09-13 11:02:25 +0200

    added feul

--- src/components/MusicManager.tsx
+++ src/components/MusicManager.tsx
@@ -1,19 +1,17 @@
 "use client";
-import React from 'react';
-
-
+import React from "react";
 
 const MusicManager: React.FC = () => {
   return (
-    <div className='flex ' >
-      <iframe className='fixed bottom-0'
-        src="https://open.spotify.com/embed/album/2w6DV9mIOPHldiUheAN2RD?utm_source=generator"
-        width="300" 
-        height="300" 
+    <div className="flex absolute ">
+      <iframe
+        src="https://open.spotify.com/embed/track/6FUwPb4mGlUDbx42uspXaZ?utm_source=generator&theme=0"
+        width="300px"
+        height="152"
         frameBorder="0"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
       ></iframe>
-
     </div>
   );
 };
