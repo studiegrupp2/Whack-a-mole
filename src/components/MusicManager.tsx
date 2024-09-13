d60e90b474fb294024d126f38be167796861c9f6
IsakGunnardo <127835187+IsakGunnardo@users.noreply.github.com>
2024-09-13 11:06:37 +0200

    added fuel

--- src/components/MusicManager.tsx
+++ src/components/MusicManager.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const MusicManager: React.FC = () => {
   return (
-    <div className="flex absolute ">
+    <div className="flex absolute top-[90dvh] ">
       <iframe
         src="https://open.spotify.com/embed/track/6FUwPb4mGlUDbx42uspXaZ?utm_source=generator&theme=0"
         width="300px"
-        height="152"
+        height="100"
         frameBorder="0"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
