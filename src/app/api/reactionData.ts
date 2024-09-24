6a52d7277eb5d3c39f5c2009507ad830ff00beb6
evelinanilsson <evelina.nilsson@mail.com>
2024-09-24 14:32:06 +0200

    format document

--- src/app/api/reactionData.ts
+++ src/app/api/reactionData.ts
@@ -1,31 +1,26 @@
 import { sql } from "@vercel/postgres";
 
-interface ReactionProp{
-    name: string;
-    reactionTime: number;
-  }
+interface ReactionProp {
+  name: string;
+  reactionTime: number;
+}
 
-  export default async function ReactionFetch(): Promise<ReactionProp[]> {
-    try {
-      // Fetch all scores
-      const result = await sql`SELECT * FROM ReactionTime ORDER BY reactiontime DESC`;
-      console.log("All ReactionTime:", result.rows);
-      console.log(result)
-      
-      const reactionResult: ReactionProp[] = result.rows.map((row) => ({
-        name: row.name,
-        reactionTime: Number(row.reactiontime),
-      }))
-  
-      return reactionResult || [];
-      
-      
-    } catch (error) {
-      console.error("Something went wrong");
-      return [];
-    }
-  }
-  
+export default async function ReactionFetch(): Promise<ReactionProp[]> {
+  try {
+    // Fetch all scores
+    const result =
+      await sql`SELECT * FROM ReactionTime ORDER BY reactiontime DESC`;
+    console.log("All ReactionTime:", result.rows);
+    console.log(result);
 
-  
-  
+    const reactionResult: ReactionProp[] = result.rows.map((row) => ({
+      name: row.name,
+      reactionTime: Number(row.reactiontime),
+    }));
+
+    return reactionResult || [];
+  } catch (error) {
+    console.error("Something went wrong");
+    return [];
+  }
+}
